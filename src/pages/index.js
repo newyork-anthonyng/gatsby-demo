6257bd012bb154582042073a8a20b85c2778e800
Anthony Ng <newyork.anthonyng@gmail.com>
2019-12-02 20:24:34 -0500

    Add commits

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,11 @@
 import React from "react"
+import Header from '../components/header';
+import { Link } from 'gatsby'
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div>
+    <Link to="/contact/">Contact</Link>
+    <Header headerText="Home" />
+    Hello Gatsby!!
+  </div>
+)
