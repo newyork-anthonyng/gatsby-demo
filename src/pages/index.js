2cce576d6c611a8a791e76dc007a28e36facaafa
Anthony Ng <newyork.anthonyng@gmail.com>
2019-12-02 20:39:58 -0500

    use css modules

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import { Link } from 'gatsby'
 export default () => (
   <div>
     <Link to="/contact/">Contact</Link>
+    <Link to="/about-css-modules/">CSS Modules</Link>
     <Header headerText="Home" />
     Hello Gatsby!!
   </div>
